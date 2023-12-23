5b3e02442977d7f760b18f31ff5e58b23da23c27
Abhishekchauhan23 <54811332+Abhishekchauhan23@users.noreply.github.com>
2023-12-23 14:37:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basicandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.basicandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Activity
+//Services
+//Intent-Filter
+//Broadcast
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
